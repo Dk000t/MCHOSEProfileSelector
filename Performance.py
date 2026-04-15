095fe36b6ed60e61c73a9fecf3535362c82902ae
Dk000t <francesco.porritiello001@studenti.uniparthenope.it>
2026-04-15 22:25:26 +0200

    Add scripts and README

---
+++ Performance.py
@@ -0,0 +1,58 @@
+ #!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Mode to select on the webpage
+MODE = "Performance"
+
+# URL to navigate to
+URL = "https://www.mchose.com.cn/#/detail?deviceName=MCHOSE+A7+V2+Ultra"
+
+# Path to the user's home directory
+HOME = os.path.expanduser("~")
+
+# Path to the Chrome user profile for persistent session
+CHROME_PROFILE_PATH = f"{HOME}/.config/chromium_profile"
+
+# Initialize Chrome options object
+options = Options()
+
+# Set the path to the Chrome user data directory, so Chrome uses this profile's data (cookies, bookmarks, extensions, etc.)
+options.add_argument(f"user-data-dir={CHROME_PROFILE_PATH}")
+
+# Set up Chrome options
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+
+try:
+    # Initialize the Chrome WebDriver with the specified options
+    driver = webdriver.Chrome(options=options)
+    
+    # Open the specified URL
+    driver.get(URL)
+    
+    # Wait up to 15 seconds for the element matching MODE text to be clickable
+    element = WebDriverWait(driver, 15).until(
+        EC.element_to_be_clickable((By.XPATH, f"//*[normalize-space()='{MODE}']"))
+    )
+    
+    # Click the element once it is clickable
+    element.click()
+    
+finally:
+    # Capture command-line arguments passed to the script
+    cmd = sys.argv[1:]
+    
+    # Run the external command
+    subprocess.run(cmd)
+    
+    # Quit the browser and clean up resources
+    driver.quit()
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # MCHOSEProfileSelector
-Script to automate the selection of MCHOSE mouse profiles.
+
+These scripts let you automatically apply a performance profile (including motion sync, polling rate, DPI, etc.) when launching a Steam game on Linux, and switch to a power-saving profile when closing it.
+
+## Instructions
+
+### Create the chromium user directory:
+```bash
+mkdir -p $HOME/.config/chromium_profile
+```
+### Start Chromium using the created user directory:
+```bash
+chromium --user-data-dir=$HOME/.config/chromium_profile
+```
+### Open the MCHOSE webdriver website, pair your mouse and create two profiles, one called "Performance" and one called "Powersave":
+```bash
+www.mchose.com.cn
+```
+### Copy the scripts to any folder you like and make scripts executable:
+```bash
+chmod +x Performance.py Powersave.py
+```
+### Add to Steam Game Commands (change the directory based on where your scripts are):
+```bash
+python3 ~/Scripts/Performance.py %command%; python3 ~/Scripts/Powersave.py
+```
